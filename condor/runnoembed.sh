496e041811ca7c73d380926cca4ae41df8ae150a
Matthew Posik <posik@temple.edu>
2023-02-07 12:24:58 -0500

    add condor files

---
+++ condor/runnoembed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#export baseDIR=/eic/u/mposik/Detector1/EPIC
+#export codeDIR=${baseDIR}/epic
+export outDIR=/gpfs/mnt/gpfs02/eic/mposik/ePIC
+#cd ${baseDIR}
+#pwd
+#./eic-shell
+#cd ${codeDIR}
+#pwd
+#source install/setup.sh
+#echo "running..."
+echo $DETECTOR
+
+echo ddsim --steeringFile steering.py --compactFile epic_tracking_only.xml -G -N 1000 --outputFile $outDIR/test.edm4hep.root
+ddsim --steeringFile steering.py --compactFile epic_tracking_only.xml -G -N 1000 --outputFile $outDIR/test.edm4hep.root
